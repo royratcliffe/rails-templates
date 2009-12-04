4f4c5d51e17b8d26cf26a6f4462db26ec7a5d295
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2009-12-04 08:50:36 +0000

    Header comment; removed welcome controller

--- base.rb
+++ base.rb
@@ -1,3 +1,13 @@
+# Add this to your Bash profile
+#
+# function railsapp
+# {
+#   path_to_your_app="$1"
+#   name_of_template="$2"
+#   shift 2
+#   rails "$path_to_your_app" -m "http://github.com/royratcliffe/rails-templates/raw/master/$name_of_template.rb" $@
+# }
+
 # Remove index.html and rails.png before firing up the Git repo
 run '>README'
 run 'rm public/index.html'
@@ -26,7 +36,3 @@ git :add => '.'
 # initial commit. Doing this step with a single invocation of git fails to
 # commit.
 git :commit => '-m "Initial commit"'
-
-generate :controller, 'welcome index'
-route "map.root :controller => 'welcome'"
-git :add => '.', :commit => '-m "Welcome controller added"'
